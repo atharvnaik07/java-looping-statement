282b947e81269e58b6ae9894c95ba80a8938d99b
Atharv Naik <114814921+atharvnaik07@users.noreply.github.com>
2024-01-08 22:53:19 +0530

    By Atharv

---
+++ do while loop.java
@@ -0,0 +1,13 @@
+// do while loop
+public class Main {
+    public static void main(String[] args) {
+        int i=1;
+
+            do {
+                System.out.println("this is do while loop");
+                System.out.println("when the condition is false that time do while loop run 1 time only");
+                i++;
+            }while(i<=10);
+
+    }
+}
---
+++ while loop.java
@@ -0,0 +1,15 @@
+// while loop
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("creating 14 table using while loop");
+       int y=14 , i=1;
+        while (i<=10){
+            int x=y*i;
+            System.out.println("2 " +i+ " sa "+x);
+            i++;
+
+        }
+
+
+    }
+}
